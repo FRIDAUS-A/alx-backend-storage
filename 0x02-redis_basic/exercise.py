6ee15ac54c70536026508039beaa5d00e74e739b
FRIDAUS <fridausokoya@gmail.com>
2024-10-23 15:00:28 +0100

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,6 +15,7 @@ class Cache:
             initialize the class Cache
         """
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: str | bytes | float | int) -> str:
         """
